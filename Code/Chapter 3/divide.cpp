3bf7022a4977fd11d2141e78e625e052fe3a369d
Hsiang-Cheng Yang <rick68@users.noreply.github.com>
2018-09-10 07:00:54 +0800

    Update divide.cpp
    fix some typos

--- Code/Chapter 3/divide.cpp
+++ Code/Chapter 3/divide.cpp
@@ -8,9 +8,9 @@ int main()
     cout << "Floating-point division: 9.0/5.0 = ";
     cout << 9.0 / 5.0 << endl;
     cout << "Mixed division: 9.0/5 = " << 9.0 / 5  << endl;
-    cout << "double constants: 1e7/9.0 = ";
+    cout << "double constants: 1.e7/9.0 = ";
     cout << 1.e7 / 9.0 <<  endl;
-    cout << "float constants: 1e7f/9.0f = ";
+    cout << "float constants: 1.e7f/9.0f = ";
     cout << 1.e7f / 9.0f <<  endl;
     // cin.get();
     return 0;
